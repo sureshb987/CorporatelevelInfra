762d0f4c56794d1d63a158623c2ce44bf73e735d
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-06-16 18:51:20 +0530

    Update main.tf

--- ec2baseapp/ec2/Modules/modules/main.tf
+++ ec2baseapp/ec2/Modules/modules/main.tf
@@ -1,3 +1,5 @@
+
+
 provider "aws" {
   region = "ap-south-1"
 }
@@ -15,4 +17,43 @@ resource "aws_instance" "corporateproject_ec2" {
     Name = "corporateproject"
   })
 }
+resource "aws_lb_target_group" "corporateproject_tg" {
+  name        = "corporateproject-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
 
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+  }
+
+  tags = merge(var.tags, {
+    Name = "corporateproject-tg"
+  })
+}
+resource "aws_launch_template" "corporateproject_lt" {
+  name_prefix   = "corporateproject-lt-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  network_interfaces {
+    associate_public_ip_address = var.associate_public_ip
+    security_groups             = var.security_group_ids
+    subnet_id                   = var.subnet_id
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge(var.tags, {
+      Name = "corporateproject"
+    })
+  }
+}

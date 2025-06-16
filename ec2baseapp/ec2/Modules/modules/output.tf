a618f445a2fe39ef09f66fe0a79b76c22346b9d3
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-06-16 18:51:53 +0530

    Update output.tf

--- ec2baseapp/ec2/Modules/modules/output.tf
+++ ec2baseapp/ec2/Modules/modules/output.tf
@@ -1,18 +1,25 @@
 output "server_ssh_access" {
-  description = "SSH access command for the Jenkins server"
-  value       = "${var.ssh_user}@${aws_instance.jenkins_server.public_ip}"
+  value = "${var.ssh_user}@${aws_instance.corporateproject_ec2.public_ip}"
 }
 
 output "public_ip" {
-  description = "Public IP of the Jenkins server"
-  value       = aws_instance.jenkins_server.public_ip
+  value = aws_instance.corporateproject_ec2.public_ip
 }
 
 output "private_ip" {
-  description = "Private IP of the Jenkins server"
-  value       = aws_instance.jenkins_server.private_ip
+  value = aws_instance.corporateproject_ec2.private_ip
 }
-output "ec2_instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.corporateproject_ec2.id
+output "target_group_arn" {
+  description = "ARN of the created target group"
+  value       = aws_lb_target_group.corporateproject_tg.arn
+}
+
+output "launch_template_id" {
+  description = "ID of the created launch template"
+  value       = aws_launch_template.corporateproject_lt.id
+}
+
+output "launch_template_name" {
+  description = "Name of the launch template"
+  value       = aws_launch_template.corporateproject_lt.name
 }

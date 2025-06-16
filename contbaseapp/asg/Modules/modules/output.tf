762192110c642b460c77ab316076403aa8caf1cf
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-06-16 19:02:56 +0530

    Update output.tf

--- contbaseapp/asg/Modules/modules/output.tf
+++ contbaseapp/asg/Modules/modules/output.tf
@@ -1,10 +1,10 @@
 output "asg_name" {
-  value       = aws_autoscaling_group.this.name
+  value       = aws_autoscaling_group.corporateproject.name
   description = "Name of the Auto Scaling Group"
 }
 
 output "asg_arn" {
-  value       = aws_autoscaling_group.this.arn
+  value       = aws_autoscaling_group.corporateproject.arn
   description = "ARN of the Auto Scaling Group"
 }
 

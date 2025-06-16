415fb83d00c5e139d23d75164bfef93f2495303c
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-06-16 18:59:48 +0530

    Update main.tf

--- contbaseapp/kms/Modules/modules/main.tf
+++ contbaseapp/kms/Modules/modules/main.tf
@@ -1,10 +1,27 @@
 provider "aws" {
   region = "ap-south-1"
 }
+data "aws_caller_identity" "current" {}
+
 resource "aws_kms_key" "corporateproject" {
-  description             = var.description
-  enable_key_rotation     = var.enable_key_rotation
-  policy                  = var.policy
+  description         = var.description
+  enable_key_rotation = var.enable_key_rotation
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "kms-policy"
+    Statement = [
+      {
+        Sid       = "AllowRootAccount"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action    = "kms:*"
+        Resource  = "*"
+      }
+    ]
+  })
 
   tags = merge(var.tags, {
     Name = "corporateproject-kms"
@@ -12,7 +29,6 @@ resource "aws_kms_key" "corporateproject" {
 }
 
 resource "aws_kms_alias" "corporateproject" {
-  name          = var.key_alias
+  name          = "alias/${var.key_alias}"
   target_key_id = aws_kms_key.corporateproject.key_id
 }
-
